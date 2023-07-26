3de956452916bde82d8a8b9d552a3dc85a5b692a
Ahmed Mahmoud <123884211+ahmeddjaxonn@users.noreply.github.com>
2023-07-26 20:14:40 +0300

    Assignments
    Assign 1 - 2 Solved

---
+++ assign_01_problem_01_code_.cpp
@@ -0,0 +1,13 @@
+//Ahmed Mahmoud 
+//20216742
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main() {
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << endl;
+    return 0;
+}
